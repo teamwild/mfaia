a4353ac426baf533eb928cfccf757ee20276f742
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-08-24 22:06:29 +0530

    Add files via upload

---
+++ spambot/helpers/callbackQuery.py
@@ -0,0 +1,90 @@
+from spambot import *
+from spambot.help import *
+from spambot.helpers.commands import *
+from telethon import events
+
+
+@MafiaBot.on(events.CallbackQuery(data=b'alive'))
+async def alive(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{ALIVE_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'ping'))
+async def ping(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{PING_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'raid'))
+async def raid(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{RAID_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'replyraid'))
+async def replyraid(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{REPLYRAID_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'spam'))
+async def spam(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{SPAM_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'pspam'))
+async def pspam(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{PSPAM_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'extras'))
+async def extras(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{EXTRA_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'tm'))
+async def tm(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{TM_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'ba'))
+async def ba(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{BA_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'dm'))
+async def ba(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{DM_RAID_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'heroku'))
+async def heroku(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit(f"{HEROKU_CMD}", buttons=BACK)
+
+@MafiaBot.on(events.CallbackQuery(data=b'back'))
+async def back(e):
+    if e.query.user_id not in MY_USERS:
+        await e.answer("Only Owner, Co-Owner And Sudo Users Can Access This Buttons!", cache_time=0, alert=True)
+    else:
+        await e.edit("This Is Help Command!!!", buttons=Buttons)
+ 
---
+++ spambot/helpers/commands.py
@@ -0,0 +1,100 @@
+
+ALIVE_CMD = """
+Check Bot Alive Or What!
+
+✮ `/alive` - To Check Am I Alive! - `(SUDO:- True)`
+"""
+
+PING_CMD =  """
+Check Bot Speed Using Ping!
+
+✮ `/ping` - To Check My Ping! - `(SUDO:- True)`
+"""
+
+RAID_CMD = """
+To Raid On Enemies!
+
+✮ `/mraid` - To Start Mention Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
+
+✮ `/sraid` - To Stop Mention Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
+
+✮ `/raid` - To Start Raid On Enemy! - `(SUDO:- True)`
+
+✮ `/draid` - To  Stop Raid On Enemy! - `(SUDO:- True)`
+"""
+
+REPLYRAID_CMD = """
+To Reply Raid On Enemies!
+
+✮ `/replyraid` - To Start Reply Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
+
+✮ `/dreplyraid` - To Stop Reply Raid On Enemies! Use In Reply Of User Message Or Give Username! - `(SUDO:- True)`
+"""
+
+SPAM_CMD = """
+To Spam Messages In Groups!
+
+✮ `/spam` - Give A Text Or Reply To Message To Spam In Range Of 99 Messages! - `(SUDO:- True)`
+
+✮ `/bigspam` - Give A Text Or Reply To Message To Big Spam In Range Of 9999 Messages! - `(SUDO:- True)`
+
+✮ `/mspam` - Reply To A Media To Spam In Range Of 99999 Messages! - `(SUDO:- True)`
+
+✮ `/uspam` - Give A Text Or Reply To Message To Unlimited Spam Messages! - `(SUDO:- True)`
+
+✮ `/ustop` - To Stop Unlimited Spam Messages! - `(SUDO:- True)`
+"""
+
+PSPAM_CMD = """
+To Spam Porn In Groups!
+
+✮ `/pspam` - To Spam Porn  In Groups! Also You Can Put Text In Caption Giving Text Or Replying To Message! - `(SUDO:- True)`
+
+✮ `/pstop` - To Stop Porn Spam! - `(SUDO:- True)`
+"""
+
+EXTRA_CMD = """
+To Add Extra Modules In Spam Bot! Only Work In Master Bot (Master Bot: Which Running On Bot Token No. 1!)!
+
+✮ `/install` - To Install Extra Plugin In SpamBot! - `(SUDO:- False)`
+
+✮ `/uninstall` - To Uninstall Plugin In SpamBot! - `(SUDO:- False)`
+"""
+
+TM_CMD = """
+To Get Media Link On Telegram!
+
+✮ `/tm` - To Get Telegraph Link Of Image And Gif! - `(SUDO:- True)`
+"""
+
+BA_CMD = """
+To Ban-Unban All Members From Groups!
+
+✮ `/banall` - To Ban All Members From Group! - `(SUDO:- True)`
+
+✮ `/unbanall` - To Unban All Members From Group! - `(SUDO:- True)`
+"""
+
+DM_RAID_CMD = """
+To Fuck Victim In His Dm!
+
+✮ `/dmraid` - To Raid In Victims Dm! - `(SUDO:- True)`
+
+✮ `/sdmraid` - To Stop Raid In Victims Dm! - `(SUDO:- True)`
+
+✮ `/dmspam` - To Spam In Victims Dm! Just Reply To Your Message And Mention Victims Username To Spam Replied Message! - `(SUDO:- True)`
+
+✮ `/sdmspam` - To Stop Spam In Victims Dm! - `(SUDO:- True)`
+
+✮ `/dmpspam` - To Porn Spam In Victims Dm! Reoly To Your Message To Store In Caption Of Porn Spam To Send In Victims Dm! - `(SUDO:- True)`
+
+✮ `/dmpstop` - To Stop Porn Spam In Victims Dm! - `(SUDO:- True)`
+"""
+
+HEROKU_CMD = """
+To Access Heroku Features Using Bot!
+
+✮ `/restart` - To Restart All Dynos Of Mafia Spam Userbot! - `(SUDO:- True)`
+
+✮ `/logs` - To Get Heroku Logs Of Mafia Spam Userbot! - `(SUDO:- True)`
+"""
