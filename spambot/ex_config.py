78469011bfd610c40a984000358cd4e3f84f458a
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-08-31 12:12:23 +0530

    Add files via upload

---
+++ spambot/ex_config.py
@@ -0,0 +1,23 @@
+import os
+
+class Config(object):
+    API_ID = 2184829
+    API_HASH = "6930b92388baabff4cb4a1d377085035"
+    SESSION_ONE = ""
+    SESSION_TWO = ""
+    SESSION_THREE = ""
+    SESSION_FOUR = ""
+    SESSION_FIVE = ""
+    BOT_TOKEN  = ""
+    BOT_USERNAME  = ""
+    OWNER_ID = ""
+    OWNER_NAME = ""
+    OWNER_USERNAME = ""
+    CO_OWNER_ID = set(1174290051, 2066333634, 5140310878) # Replace with your user id
+    SUDO_USERS = set(1174290051, 2066333634, 5140310878) # Replace with your user id
+    HEROKU_APP_ID = ""
+    HEROKU_API_KEY = ""
+    DISPLAY_PIC = ""
+    BIO_MSG = ""
+    HEROKU_API_KEY = ""
+    HEROKU_APP_NAME = ""
